276cf88f3d6556a2d48983ad1f61d27ea7a31daf
Manakal <dp.manakal82@gmail.com>
2022-04-24 23:48:26 +0530

    Upload working with deafult antd upload

--- src/Dashboard/DashBoardOptions/modals/AddKbFormModal.js
+++ src/Dashboard/DashBoardOptions/modals/AddKbFormModal.js
@@ -1,20 +1,93 @@
-import { Modal, Button } from "antd";
+import { Modal, Button, Form, Select, Upload, message } from "antd";
 import { useState } from "react";
+import Input from "antd/es/input/Input";
+import Checkbox from "antd/es/checkbox/Checkbox";
+import { Option } from "antd/es/mentions";
+import { UploadOutlined } from "@ant-design/icons";
+
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
+const layout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 4, span: 16 },
+};
 
 const AddKbFormModal = (props) => {
   const [confirmLoading, setConfirmLoading] = useState(false);
   const [modalText, setModalText] = useState("Content of the modal");
+  const [fileName, setFileName] = useState("");
+
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  const onFill = () => {
+    form.setFieldsValue({
+      note: "Hello world!",
+      gender: "male",
+    });
+  };
+
+  const fileProps = {
+    name: "file",
+    action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+    headers: {
+      authorization: "authorization-text",
+    },
+    onChange(info) {
+      if (info.file.status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === "done") {
+        message.success(`${info.file.name} file added successfully`);
+      } else if (info.file.status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
 
   return (
     <Modal
-      title="Basic Modal"
+      title="Add KnowledgeBase"
       visible={props.visibility}
       onOk={() => props.handleOkBtnClicked()}
       onCancel={() => props.handleOkBtnClicked()}
     >
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+        <Form.Item name="note" label="File Name" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item name="URI" label="URI" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Upload
+            action={"http://localhost:5000/api/upload"}
+            beforeUpload={(file) => {
+              console.log(file);
+            }}
+            data={(response) => {
+              console.log(response.response);
+            }}
+          >
+            <Button icon={<UploadOutlined />}>Click to Upload</Button>
+          </Upload>
+        </Form.Item>
+      </Form>
     </Modal>
   );
 };
--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -12,89 +12,60 @@ import {
   UserOutlined,
 } from "@ant-design/icons";
 import { Link, Outlet, useNavigate, useLocation } from "react-router-dom";
-// import {  } from "react-router-dom";
+
 import { getallKB } from "../API/api";
 import axios from "axios";
 
 const { SubMenu } = Menu;
-// function SubMenu(props) {
-//   return null;
-// }
-
 SubMenu.propTypes = {
   title: PropTypes.string,
   icon: PropTypes.element,
   children: PropTypes.node,
 };
 
-function HeaderView() {
-  const location = useLocation();
-  console.log(location.pathname);
-  return location.pathname;
-}
-//
-// const items = [
-//   { key: "1", label: "Home", path: "/Dashboard", icon: <HomeOutlined /> },
-//   {
-//     key: "2",
-//     label: "KnowledgeBase",
-//     path: "KnowledgeBase",
-//     icon: <ReadOutlined />,
-//   },
-//   // {
-//   //   key: "3",
-//   //   label: "Service Contract Details",
-//   //   path: "/admin/service-contract-details",
-//   // },
-//   // { key: "4", label: "Cost Centers", path: "/admin/cost-centers" },
-//   // { key: "5", label: "Clients", path: "/admin/clients" },
-//   // { key: "6", label: "Vendors", path: "/admin/vendors" },
-// ];
-
 const Dashboard = () => {
   const [collapsed, setCollapsed] = useState(false);
   const [selectedKey, setSelectedKey] = useState("");
-  // const [items, setItems] = useState([
-  //   { key: "1", label: "Home", path: "/Dashboard", icon: <HomeOutlined /> },
-  //   {
-  //     key: "2",
-  //     label: "KnowledgeBase",
-  //     path: "KnowledgeBase",
-  //     icon: <ReadOutlined />,
-  //   },
-  // ]);
+  function HeaderView() {
+    const location = useLocation();
+    console.log(location.pathname);
+    return location.pathname;
+  }
+  useEffect(() => {
+    const path = window.location.pathname;
+    const buttonIndex = highlightbtton(path);
+    return () => {
+      changeSelectedKey(buttonIndex);
+    };
+  }, []);
+
+  const changeSelectedKey = (key) => {
+    setSelectedKey(key);
+  };
+
+  let highlightbtton = (path) => {
+    const pathname = path.split("/");
+    const realPath = pathname[pathname.length - 1];
+    switch (realPath) {
+      case "Dashboard":
+        return "1";
+        break;
+      case "Dashboard":
+        return "1";
+        break;
+    }
+    return "";
+  };
+
   const toggleCollapsed = () => {
     setCollapsed(!collapsed);
   };
-  //
-  // const location = useLocation();
-  // const navigate = useNavigate();
-  //
-  // const [selectedKey, setSelectedKey] = useState(
-  //   items.find((_item) => location.pathname.startsWith(_item.path)).key
-  // );
-  //
-  // const onClickMenu = (item) => {
-  //   const clicked = items.find((_item) => _item.key === item.key);
-  //   navigate(clicked.path);
-  // };
-
-  // useEffect(() => {
-  //   setSelectedKey(
-  //     items.find((_item) => location.pathname.startsWith(_item.path)).key
-  //   );
-  // }, [location]);
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
       <Sider collapsible collapsed={collapsed} onCollapse={toggleCollapsed}>
         <div className="logo" />
         <Menu theme="dark" selectedKeys={selectedKey} mode="inline">
-          {/*/!*{items.map((item) => (*!/*/}
-          {/*/!*  <Menu.Item key={item.key} icon={item.icon} onClick={onClickMenu}>*!/*/}
-          {/*/!*    {item.label}*!/*/}
-          {/*/!*  </Menu.Item>*!/*/}
-          {/*))}*/}
           <Menu.Item
             key="1"
             icon={<HomeOutlined />}
@@ -111,38 +82,17 @@ const Dashboard = () => {
             KnowledgeBase
             <Link to="KnowledgeBase" />
           </Menu.Item>
-          {/*<Menu.Item key="3" icon={<MessageOutlined />}>*/}
-          {/*  Intents*/}
-          {/*</Menu.Item>*/}
-          {/*<SubMenu key="sub1" icon={<UserOutlined />} title="User">*/}
-          {/*  <Menu.Item key="4">Bill</Menu.Item>*/}
-          {/*  <Menu.Item key="5">Alex</Menu.Item>*/}
-          {/*</SubMenu>*/}
-          {/*<SubMenu key="sub2" icon={<TeamOutlined />} title="Team">*/}
-          {/*  <Menu.Item key="6">Team 1</Menu.Item>*/}
-          {/*  <Menu.Item key="7">Team 2</Menu.Item>*/}
-          {/*</SubMenu>*/}
         </Menu>
       </Sider>
       <Layout className="site-layout">
         <Content style={{ margin: "0 16px" }}>
           <Breadcrumb style={{ margin: "16px 0" }}>
             <Breadcrumb.Item>{HeaderView()}</Breadcrumb.Item>
-            {/*<UserOutlined style={{ fontSize: "30px", marginLeft: "28em" }} />*/}
-            {/*<h6 style={{ marginLeft: "73em" }}>Admin User</h6>*/}
           </Breadcrumb>
           <div
             className="site-layout-background"
             style={{ padding: 24, minHeight: 660 }}
           >
-            {/*<Routes>*/}
-            {/*  <Route>*/}
-            {/*    <Route*/}
-            {/*      path="Dashboard/KnowledgeBase"*/}
-            {/*      component={KnowledgeBase}*/}
-            {/*    />*/}
-            {/*  </Route>*/}
-            {/*</Routes>*/}
             <Outlet />
           </div>
         </Content>
